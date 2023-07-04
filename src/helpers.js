2a6ea8a33bef7c0d35efbafaeece6d37f5a664cc
Yulia <78503488+Yuliteg@users.noreply.github.com>
2023-07-04 12:12:24 +0200

    refactor-code

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,15 @@
 export const onlyOneTypeRegex = /^([a-zA-Z]+|[0-9]+|[^a-zA-Z0-9]+)$/;
 export const combinationRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]|(?=.*[A-Za-z])(?=.*[@$!%*#?&+=._-])[A-Za-z@$!%*#?&+=._-]|(?=.*\d)(?=.*[@$!%*#?&+=._-])[\d@$!%*#?&+=._-]+$/;
 export const lettersSymbolsNumbersRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&+=._-])[A-Za-z\d@$!%*#?&+=._-]+$/;
+
+export const strengthClasses = {
+  weak: 'line red',
+  medium: 'line yellow',
+  strong: 'line green',
+};
+
+export const strengthMessages = {
+  weak: "Your password must contain more than 8 characters!",
+  medium: "Your string must contain letters, numbers and symbols!",
+  strong: '',
+};
--- src/passContext.js
+++ src/passContext.js
@@ -1,12 +1,9 @@
 import { createContext, useContext, useMemo, useState } from "react";
-import { onlyOneTypeRegex, lettersSymbolsNumbersRegex, combinationRegex } from './helpers';
+import { onlyOneTypeRegex, lettersSymbolsNumbersRegex, combinationRegex, strengthMessages, strengthClasses } from './helpers';
 
 const GlobalContext = createContext();
 
 export const useGlobalContext = () => useContext(GlobalContext);
-
-let weakMessage = 'Your password must contain more than 8 characters!';
-let easyMessage = 'Your string must contain letters, numbers and symbols!';
 
 const PassContext = ({ children }) => {
   const [password, setPassword] = useState('');
@@ -14,35 +11,40 @@ const PassContext = ({ children }) => {
   const [message, setMessage] = useState('');
 
   const calculateStrength = useMemo(() => {
-    if (password.length <= 8 && password.length > 0) {
-      setClassName('line red');
-      setMessage(weakMessage);
-      return 'weak';
-    } else if (password.length > 8) {
-      if (lettersSymbolsNumbersRegex.test(password)) {
-        setClassName('line green');
-        setMessage('');
-        return 'strong';
-      } else if (combinationRegex.test(password)) {
-        setClassName('line yellow');
-        setMessage(easyMessage);
-        return 'medium';
-      } else if (onlyOneTypeRegex.test(password)) {
-        setClassName('line red');
-        setMessage(easyMessage);
-        return 'easy';
-      }
-    } else {
+    const passwordLength = password.length;
+
+    if (passwordLength === 0) {
       setClassName('line');
       setMessage('');
       return '';
     }
+    if (passwordLength <= 8) {
+      setClassName(strengthClasses.weak);
+      setMessage(strengthMessages.weak);
+      return 'weak';
+    }
+
+    if (lettersSymbolsNumbersRegex.test(password)) {
+      setClassName(strengthClasses.strong);
+      setMessage(strengthMessages.strong);
+      return 'strong';
+    }
+
+    if (combinationRegex.test(password)) {
+      setClassName(strengthClasses.medium);
+      setMessage(strengthMessages.medium);
+      return 'medium';
+    }
+
+    setClassName(strengthClasses.weak);
+    setMessage(strengthMessages.medium);
+    return 'easy';
   }, [password])
 
   return (
-   <GlobalContext.Provider value={{password, setPassword, className, setClassName, message, setMessage, calculateStrength}}>
-     {children}
-   </GlobalContext.Provider>
+    <GlobalContext.Provider value={{ password, setPassword, className, setClassName, message, setMessage, calculateStrength }}>
+      {children}
+    </GlobalContext.Provider>
   )
 }
 
